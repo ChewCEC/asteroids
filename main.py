c9c185cfd2c2c874619cd5969ce30c170b42234d
ChewCEC <camilo.echeverri.100@gmail.com>
2024-10-10 17:38:08 -0500

    add Player class, payer now can rotate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 def main():
     pygame.init()
     delta_time = pygame.time.Clock()
@@ -8,13 +8,25 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # Loop principal del juego
     running = True
+
+    # Create player
+    x_coord = SCREEN_WIDTH / 2
+    y_coord = SCREEN_HEIGHT / 2
+    player = Player(x_coord, y_coord)
+
     while running:
         screen.fill((0, 0, 0))
+        player.draw(screen)
+        player.update(dt)
+
+        # render screen should be always at last, if not none of the objects will display
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False 
         dt = delta_time.tick(144)/1000
+
+
     pygame.quit()
 
 if __name__ == '__main__':
