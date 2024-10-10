85455e382ec9ac7addfc9dbd35eb1337bfeaa07c
ChewCEC <camilo.echeverri.100@gmail.com>
2024-10-09 20:20:34 -0500

    add Delta time clock

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import pygame
 from constants import *
 
-print("Starting asteroids!")
-print(f'Screen width: {SCREEN_WIDTH}')
-print(f'Screen height: {SCREEN_HEIGHT}')
-
-
-if __name__ == "__main__":
+def main():
     pygame.init()
+    delta_time = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
+    # Loop principal del juego
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False 
+        dt = delta_time.tick(144)/1000
+    pygame.quit()
 
-    #main()
+if __name__ == '__main__':
+    print("Starting asteroids!")
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
+    main()
